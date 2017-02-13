9f271798a9925cf9015f50eedb06fabeb1d420b6
3xp10it <3xp10it@users.noreply.github.com>
2017-02-13 16:39:34 +0800

    Update xwaf.py

--- xwaf.py
+++ xwaf.py
@@ -179,7 +179,7 @@ class Program(object):
                 else:
                     newString+=(" "+each)
 
-            newCommand="python3 xwaf.py"+newString
+            newCommand="python3 %sxwaf.py" % currentScriptDirPath+newString
             print("new cmd:\n"+newCommand)
             os.system(newCommand)
             sys.exit(0)
